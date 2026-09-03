5a8ab1a6d2282267efdc50443aaaeb094ba6cb3e
Ronnie Rosal <48272231+ronnierosal@users.noreply.github.com>
2026-09-02 22:16:45 -0700

    build: normalize Decky sourcemap paths

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,15 @@
 import deckyPlugin from "@decky/rollup";
 
-export default deckyPlugin({});
+const config = deckyPlugin({});
+const deckySourcemapPathTransform = config.output.sourcemapPathTransform;
+
+// @decky/rollup expects POSIX separators when it rewrites source paths to
+// decky:// URLs. Rollup supplies native backslashes on Windows, which made the
+// committed source map differ from the one rebuilt by Linux CI.
+config.output.sourcemapPathTransform = (relativeSourcePath, sourcemapPath) =>
+  deckySourcemapPathTransform(
+    relativeSourcePath.replaceAll("\\", "/"),
+    sourcemapPath,
+  );
+
+export default config;
